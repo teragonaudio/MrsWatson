8d7793fc8f6bffc8d4d9e08288a9ca233edb2ed8
Nik Reiman <nik.reiman@gmail.com>
2011-09-25 22:36:23 +0200

    Print version number

--- source/mrswatson.py
+++ source/mrswatson.py
@@ -4,3 +4,14 @@ versionMajor=0
 versionMinor=0
 versionPatch=1
 
+def printVersion():
+    print "This is MrsWatson, version %d.%d.%d" % (versionMajor, versionMinor, versionPatch)
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-v", "--version", dest="showVersion", action="store_true",
+                  help="Print version and copyright information", default=False)
+(options, args) = parser.parse_args()
+
+if options.showVersion:
+    printVersion()
