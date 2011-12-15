e4178c89bf2d376ad1d936ba428bcf2fd28d5fc0
Nik Reiman <nik.reiman@gmail.com>
2011-12-15 10:39:23 +0100

    Adding main method, copyright

--- source/MrsWatson.c
+++ source/MrsWatson.c
@@ -3,7 +3,11 @@
 //  MrsWatson
 //
 //  Created by Nik Reiman on 12/5/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Teragon Audio. All rights reserved.
 //
 
 #include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+}
