a450d45f82fc1ea0bb03978411e891d606d19413
Nik Reiman <nik.reiman@gmail.com>
2012-01-02 11:43:00 +0100

    Adding file stubs for program arguments

---
+++ source/ProgramArgument.h
@@ -0,0 +1,14 @@
+//
+//  ProgramArgument.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/2/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_ProgramArgument_h
+#define MrsWatson_ProgramArgument_h
+
+
+
+#endif
