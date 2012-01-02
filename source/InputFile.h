130aba58106d85390118a98b2f9f052b9b1cadcb
Nik Reiman <nik.reiman@gmail.com>
2012-01-02 16:14:11 +0100

    Adding InputFile files

---
+++ source/InputFile.h
@@ -0,0 +1,14 @@
+//
+//  InputFile.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/2/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_InputFile_h
+#define MrsWatson_InputFile_h
+
+
+
+#endif
