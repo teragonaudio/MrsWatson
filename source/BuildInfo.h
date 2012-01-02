2eca9ab10bfa0e71ec866e06383ede8ce162262f
Nik Reiman <nik.reiman@gmail.com>
2012-01-02 16:57:54 +0100

    Adding files for build info

---
+++ source/BuildInfo.h
@@ -0,0 +1,14 @@
+//
+//  BuildInfo.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/2/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_BuildInfo_h
+#define MrsWatson_BuildInfo_h
+
+
+
+#endif
