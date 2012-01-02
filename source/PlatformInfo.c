61f8ea79dd1b4f378e305c30affdc162dd8670a9
Nik Reiman <nik.reiman@gmail.com>
2012-01-02 19:15:07 +0100

    Adding files for platform info

---
+++ source/PlatformInfo.c
@@ -0,0 +1,9 @@
+//
+//  PlatformInfo.c
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/2/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ source/PlatformInfo.h
@@ -0,0 +1,14 @@
+//
+//  PlatformInfo.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/2/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_PlatformInfo_h
+#define MrsWatson_PlatformInfo_h
+
+
+
+#endif
