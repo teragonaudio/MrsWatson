2bf001aabd70469694d90e5cd4b2c32d2702e6b2
Nik Reiman <nik.reiman@gmail.com>
2012-01-03 13:18:30 +0100

    Adding PluginVst2x files, MACOSX definition

---
+++ source/PluginVst2x.c
@@ -0,0 +1,9 @@
+//
+//  PluginVst2x.c
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/3/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ source/PluginVst2x.h
@@ -0,0 +1,14 @@
+//
+//  PluginVst2x.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/3/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_PluginVst2x_h
+#define MrsWatson_PluginVst2x_h
+
+
+
+#endif
