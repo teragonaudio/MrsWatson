4a694d9edf4ffa5c7a578f019aa135024ad430c7
Nik Reiman <nik.reiman@gmail.com>
2012-01-03 13:51:22 +0100

    Adding PluginChain file

---
+++ source/PluginChain.h
@@ -0,0 +1,14 @@
+//
+//  PluginChain.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/3/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_PluginChain_h
+#define MrsWatson_PluginChain_h
+
+
+
+#endif
