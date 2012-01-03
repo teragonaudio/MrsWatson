8b2f7b2eee6ed578e0d7b1c565a3d3fd6f8a0151
Nik Reiman <nik.reiman@gmail.com>
2012-01-03 16:02:17 +0100

    Classish for linked list of strings

--- source/CharStringList.c
+++ source/CharStringList.c
@@ -7,3 +7,45 @@
 //
 
 #include <stdio.h>
+#import <stdlib.h>
+#import <string.h>
+#import "CharStringList.h"
+
+CharStringList newCharStringList(void) {
+  CharStringList list = malloc(sizeof(CharStringListMembers));
+
+  list->item = NULL;
+  list->nextItem = NULL;
+
+  return list;
+}
+
+void addItemToStringList(CharStringList list, const CharString charString) {
+  CharStringListIterator iterator = list;
+  while(iterator->nextItem != NULL) {
+    iterator = iterator->nextItem;
+  }
+  iterator->item = newCharString();
+  strncpy(iterator->item, charString, STRING_LENGTH);
+  iterator->nextItem = newCharStringList();
+}
+
+int numItemsInStringList(CharStringList list) {
+  int result = 0;
+  CharStringListIterator iterator = list;
+  while(iterator != NULL) {
+    result++;
+    iterator = iterator->nextItem;
+  }
+  return result;
+}
+
+void freeCharStringList(CharStringList list) {
+  CharStringListIterator iterator = list;
+  while(iterator != NULL) {
+    free(iterator->item);
+    CharStringList current = iterator;
+    iterator = iterator->nextItem;
+    free(current);
+  }
+}
--- source/CharStringList.h
+++ source/CharStringList.h
@@ -6,9 +6,22 @@
 //  Copyright (c) 2012 Teragon Audio. All rights reserved.
 //
 
+#import "CharString.h"
+
 #ifndef MrsWatson_CharStringList_h
 #define MrsWatson_CharStringList_h
 
+typedef struct {
+  CharString item;
+  void* nextItem;
+} CharStringListMembers;
 
+typedef CharStringListMembers* CharStringList;
+typedef CharStringListMembers*CharStringListIterator;
+
+CharStringList newCharStringList(void);
+void addItemToStringList(CharStringList list, const CharString charString);
+int numItemsInStringList(CharStringList list);
+void freeCharStringList(CharStringList list);
 
 #endif
