e77eb3f359dcf9b197c6f5e8de93c77ff9f34847
Nik Reiman <nik.reiman@gmail.com>
2012-01-03 11:42:31 +0100

    Adding files for Plugin

---
+++ source/Plugin.c
@@ -0,0 +1,9 @@
+//
+//  Plugin.c
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/3/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ source/Plugin.h
@@ -0,0 +1,14 @@
+//
+//  Plugin.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/3/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_Plugin_h
+#define MrsWatson_Plugin_h
+
+
+
+#endif
