34ce6d2580d20c6a0b469aa4b4741660a6e08758
Nik Reiman <nik.reiman@gmail.com>
2012-01-04 15:14:52 +0100

    Impleentation of audio settings singleton

--- source/AudioSettings.c
+++ source/AudioSettings.c
@@ -7,3 +7,34 @@
 //
 
 #include <stdio.h>
+#import <stdlib.h>
+#import "AudioSettings.h"
+
+AudioSettings audioSettingsInstance = NULL;
+
+void initAudioSettings(void) {
+  // TODO: This is never freed, but it lives for the life of the program. Do we need to worry about that?
+  audioSettingsInstance = malloc(sizeof(AudioSettingsMembers));
+  audioSettingsInstance->sampleRate = DEFAULT_SAMPLERATE;
+  audioSettingsInstance->numChannels = DEFAULT_NUM_CHANNELS;
+  audioSettingsInstance->blocksize = DEFAULT_BLOCKSIZE;
+}
+
+AudioSettings getAudioSettings(void) {
+  return audioSettingsInstance;
+}
+
+void setSampleRate(const float sampleRate) {
+  AudioSettings audioSettings = getAudioSettings();
+  audioSettings->sampleRate = sampleRate;
+}
+
+void setNumChannels(const int numChannels) {
+  AudioSettings audioSettings = getAudioSettings();
+  audioSettings->numChannels = numChannels;
+}
+
+void setBlocksize(const int blocksize) {
+  AudioSettings audioSettings = getAudioSettings();
+  audioSettings->blocksize = blocksize;
+}
--- source/AudioSettings.h
+++ source/AudioSettings.h
@@ -9,6 +9,24 @@
 #ifndef MrsWatson_AudioSettings_h
 #define MrsWatson_AudioSettings_h
 
+#define DEFAULT_SAMPLERATE 44100.0f
+#define DEFAULT_NUM_CHANNELS 2
+#define DEFAULT_BLOCKSIZE 512
 
+typedef struct {
+  float sampleRate;
+  int numChannels;
+  int blocksize;
+} AudioSettingsMembers;
+
+typedef AudioSettingsMembers* AudioSettings;
+extern AudioSettings audioSettingsInstance;
+
+void initAudioSettings(void);
+
+AudioSettings getAudioSettings(void);
+void setSampleRate(const float sampleRate);
+void setNumChannels(const int numChannels);
+void setBlocksize(const int blocksize);
 
 #endif
