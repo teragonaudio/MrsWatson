b0c319def3bf6f1e0a802196d67fca13db00b435
Nik Reiman <nik.reiman@gmail.com>
2012-01-04 15:00:09 +0100

    Adding file for AudioSettings

---
+++ source/AudioSettings.h
@@ -0,0 +1,14 @@
+//
+//  AudioSettings.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/4/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_AudioSettings_h
+#define MrsWatson_AudioSettings_h
+
+
+
+#endif
