9f3b128e7c6c088760953b43a0ff0502d2f30fe3
Nik Reiman <nik.reiman@gmail.com>
2012-01-04 15:21:55 +0100

    Alphabetizing options

--- source/ProgramOption.c
+++ source/ProgramOption.c
@@ -35,19 +35,19 @@ ProgramOption* newProgramOptions(void) {
   ProgramOptions programOptions = malloc(sizeof(ProgramOptions) * NUM_OPTIONS);
 
   // TODO: Expand help for options
-  _addNewProgramOption(programOptions, OPTION_SAMPLERATE, "samplerate", "Set sample rate", false, ARGUMENT_TYPE_REQUIRED);
   _addNewProgramOption(programOptions, OPTION_BLOCKSIZE, "blocksize", "Set blocksize", false, ARGUMENT_TYPE_REQUIRED);
+  _addNewProgramOption(programOptions, OPTION_COLOR_LOGGING, "color", "Color-coded logging output", false, ARGUMENT_TYPE_OPTIONAL);
+  _addNewProgramOption(programOptions, OPTION_DISPLAY_INFO, "info", "Print information about the plugin(s)", false, ARGUMENT_TYPE_NONE);
+  _addNewProgramOption(programOptions, OPTION_HELP, "help", "Print help", true, ARGUMENT_TYPE_NONE);
+  _addNewProgramOption(programOptions, OPTION_INPUT_SOURCE, "input", "Input source", true, ARGUMENT_TYPE_REQUIRED);
   _addNewProgramOption(programOptions, OPTION_NUM_CHANNELS, "channels", "Set number of channels", false, ARGUMENT_TYPE_REQUIRED);
   _addNewProgramOption(programOptions, OPTION_PCM_FILE_NUM_CHANNELS, "pcm-file-channels", "Number of channels to use when reading raw PCM data", false, ARGUMENT_TYPE_REQUIRED);
   _addNewProgramOption(programOptions, OPTION_PCM_FILE_SAMPLERATE, "pcm-file-samplerate", "Sample rate to use when reading raw PCM data", false, ARGUMENT_TYPE_REQUIRED);
   _addNewProgramOption(programOptions, OPTION_PLUGIN, "plugin", "Plugin(s) to process", true, ARGUMENT_TYPE_REQUIRED);
-  _addNewProgramOption(programOptions, OPTION_DISPLAY_INFO, "info", "Print information about the plugin(s)", false, ARGUMENT_TYPE_NONE);
-  _addNewProgramOption(programOptions, OPTION_INPUT_SOURCE, "input", "Input source", true, ARGUMENT_TYPE_REQUIRED);
-  _addNewProgramOption(programOptions, OPTION_HELP, "help", "Print help", true, ARGUMENT_TYPE_NONE);
-  _addNewProgramOption(programOptions, OPTION_VERSION, "version", "Print version and copyright information", false, ARGUMENT_TYPE_NONE);
-  _addNewProgramOption(programOptions, OPTION_COLOR_LOGGING, "color", "Color-coded logging output", false, ARGUMENT_TYPE_OPTIONAL);
-  _addNewProgramOption(programOptions, OPTION_VERBOSE, "verbose", "Verbose logging", true, ARGUMENT_TYPE_NONE);
   _addNewProgramOption(programOptions, OPTION_QUIET, "quiet", "Only log critical errors", true, ARGUMENT_TYPE_NONE);
+  _addNewProgramOption(programOptions, OPTION_SAMPLERATE, "samplerate", "Set sample rate", false, ARGUMENT_TYPE_REQUIRED);
+  _addNewProgramOption(programOptions, OPTION_VERBOSE, "verbose", "Verbose logging", true, ARGUMENT_TYPE_NONE);
+  _addNewProgramOption(programOptions, OPTION_VERSION, "version", "Print version and copyright information", false, ARGUMENT_TYPE_NONE);
 
   return programOptions;
 }
@@ -157,7 +157,6 @@ boolean parseCommandLine(ProgramOptions programOptions, int argc, char** argv) {
 }
 
 void printProgramOptions(ProgramOptions programOptions) {
-  // TODO: Instead of just printing out all options, they should be alphabetized.
   for(int i = 0; i < NUM_OPTIONS; i++) {
     printf("  ");
     ProgramOption programOption = programOptions[i];
