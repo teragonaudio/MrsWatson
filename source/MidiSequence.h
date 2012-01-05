995a1422ca81c6cc583dd95122ce9ac9fa91826f
Nik Reiman <nik.reiman@gmail.com>
2012-01-05 10:49:17 +0100

    Implementation of midi sequence interface

--- source/MidiSequence.h
+++ source/MidiSequence.h
@@ -6,9 +6,18 @@
 //  Copyright (c) 2012 Teragon Audio. All rights reserved.
 //
 
+#import "LinkedList.h"
+
 #ifndef MrsWatson_MidiSequence_h
 #define MrsWatson_MidiSequence_h
 
+typedef struct {
+  LinkedList midiEvents;
+} MidiSequenceMembers;
 
+typedef MidiSequenceMembers* MidiSequence;
+
+MidiSequence newMidiSequence(void);
+void freeMidiSequence(MidiSequence midiSequence);
 
 #endif
