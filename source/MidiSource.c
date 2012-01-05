dc48becb7f0561aade45058e2ea5a62d7ca88caf
Nik Reiman <nik.reiman@gmail.com>
2012-01-05 10:32:26 +0100

    Renaming MidiFile to MidiSource

--- source/MidiSource.c
+++ source/MidiSource.c
@@ -1,14 +1,9 @@
 //
-//  MidiFile.h
+//  MidiSource.c
 //  MrsWatson
 //
 //  Created by Nik Reiman on 1/5/12.
 //  Copyright (c) 2012 Teragon Audio. All rights reserved.
 //
 
-#ifndef MrsWatson_MidiFile_h
-#define MrsWatson_MidiFile_h
-
-
-
-#endif
+#include <stdio.h>
--- source/MidiSource.h
+++ source/MidiSource.h
@@ -1,9 +1,14 @@
 //
-//  MidiFile.c
+//  MidiSource.h
 //  MrsWatson
 //
 //  Created by Nik Reiman on 1/5/12.
 //  Copyright (c) 2012 Teragon Audio. All rights reserved.
 //
 
-#include <stdio.h>
+#ifndef MrsWatson_MidiSource_h
+#define MrsWatson_MidiSource_h
+
+
+
+#endif
