707398a2518250f041087fce84a804918da79488
Nik Reiman <nik.reiman@gmail.com>
2012-01-05 10:34:17 +0100

    Adding files for MidiEvent

---
+++ source/MidiEvent.h
@@ -0,0 +1,14 @@
+//
+//  MidiEvent.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/5/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_MidiEvent_h
+#define MrsWatson_MidiEvent_h
+
+
+
+#endif
