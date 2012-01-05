9794c5136524bfeb949ab5258d3ab987af63a584
Nik Reiman <nik.reiman@gmail.com>
2012-01-05 10:33:17 +0100

    Adding file for MidiSourceFile

---
+++ source/MidiSourceFile.c
@@ -0,0 +1,9 @@
+//
+//  MidiSourceFile.c
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/5/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ source/MidiSourceFile.h
@@ -0,0 +1,14 @@
+//
+//  MidiSourceFile.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/5/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_MidiSourceFile_h
+#define MrsWatson_MidiSourceFile_h
+
+
+
+#endif
