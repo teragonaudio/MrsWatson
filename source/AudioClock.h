56407d19015197799b034ca80cbf102ed79ee4b9
Nik Reiman <nik.reiman@gmail.com>
2012-01-05 12:21:01 +0100

    Adding files for AudioClock, TaskTimer

---
+++ source/AudioClock.h
@@ -0,0 +1,14 @@
+//
+//  AudioClock.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/5/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_AudioClock_h
+#define MrsWatson_AudioClock_h
+
+
+
+#endif
---
+++ source/TaskTimer.h
@@ -0,0 +1,14 @@
+//
+//  TaskTimer.h
+//  MrsWatson
+//
+//  Created by Nik Reiman on 1/5/12.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
+//
+
+#ifndef MrsWatson_TaskTimer_h
+#define MrsWatson_TaskTimer_h
+
+
+
+#endif
