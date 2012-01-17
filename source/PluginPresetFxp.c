9fe70d6d1f7295487c81911f3096d4e515033b07
Nik Reiman <nik.reiman@gmail.com>
2012-01-17 09:39:35 +0100

    Mostly complete implementation of FXP presets

--- source/PluginPresetFxp.c
+++ source/PluginPresetFxp.c
@@ -31,6 +31,7 @@
 #include "PluginPresetFxp.h"
 #include "EventLogger.h"
 #include "PlatformUtilities.h"
+#include "PluginVst2x.h"
 
 static boolean _openPluginPresetFxp(void* pluginPresetPtr) {
   PluginPreset pluginPreset = pluginPresetPtr;
@@ -87,37 +88,82 @@ static boolean _loadPluginPresetFxp(void* pluginPresetPtr, Plugin plugin) {
   numObjectsRead = fread(&valueBuffer, sizeof(unsigned int), 1, extraData->fileHandle);
   if(numObjectsRead != 1) {
     logError("Short read of FXP preset file at version");
+    return false;
   }
-  inProgram->version = convertIntToBigEndian(valueBuffer);
+  inProgram->version = convertBigEndianIntToPlatform(valueBuffer);
 
   numObjectsRead = fread(&valueBuffer, sizeof(unsigned int), 1, extraData->fileHandle);
   if(numObjectsRead != 1) {
     logError("Short read of FXP preset file at fxID");
+    return false;
   }
-  inProgram->fxID = convertIntToBigEndian(valueBuffer);
+  inProgram->fxID = convertBigEndianIntToPlatform(valueBuffer);
 
   numObjectsRead = fread(&valueBuffer, sizeof(unsigned int), 1, extraData->fileHandle);
   if(numObjectsRead != 1) {
     logError("Short read of FXP preset file at fxVersion");
+    return false;
   }
-  inProgram->fxVersion = convertIntToBigEndian(valueBuffer);
+  inProgram->fxVersion = convertBigEndianIntToPlatform(valueBuffer);
 
   numObjectsRead = fread(&valueBuffer, sizeof(unsigned int), 1, extraData->fileHandle);
   if(numObjectsRead != 1) {
     logError("Short read of FXP preset file at numParams");
+    return false;
   }
-  inProgram->numParams = convertIntToBigEndian(valueBuffer);
+  inProgram->numParams = convertBigEndianIntToPlatform(valueBuffer);
 
   memset(inProgram->prgName, 0, sizeof(char) * 28);
   numObjectsRead = fread(inProgram->prgName, sizeof(char), 28, extraData->fileHandle);
   if(numObjectsRead != 28) {
     logError("Short read of FXP preset file at prgName");
+    return false;
   }
   copyToCharString(pluginPreset->presetName, inProgram->prgName);
 
   if(programType == FXP_TYPE_REGULAR) {
+    for(unsigned int i = 0; i < inProgram->numParams; i++) {
+      float parameterBuffer = 0.0f;
+      numObjectsRead = fread(&parameterBuffer, sizeof(float), 1, extraData->fileHandle);
+      if(numObjectsRead != 1) {
+        logError("Short read of FXP preset at parameter data");
+        return false;
+      }
+      float parameterValue = convertBigEndianFloatToPlatform(parameterBuffer);
+      plugin->setParameter(plugin, i, parameterValue);
+    }
   }
   else if(programType == FXP_TYPE_OPAQUE_CHUNK) {
+    numObjectsRead = fread(&valueBuffer, sizeof(unsigned int), 1, extraData->fileHandle);
+    if(numObjectsRead != 1) {
+      logError("Short read of FXP preset file at chunk size");
+      return false;
+    }
+    inProgram->content.data.size = convertBigEndianIntToPlatform(valueBuffer);
+    unsigned int chunkSize = inProgram->content.data.size;
+    if(chunkSize == 0) {
+      logError("FXP preset has chunk of 0 bytes");
+      return false;
+    }
+
+    char* chunk = (char*)malloc(sizeof(char) * chunkSize);
+    memset(chunk, 0, sizeof(char) * chunkSize);
+    numObjectsRead = fread(chunk, sizeof(char), chunkSize, extraData->fileHandle);
+    if(numObjectsRead != chunkSize) {
+      logError("Short read of FXP preset file at chunk");
+      return false;
+    }
+
+    // The chunk has been read, set it to the actual plugin
+    if(plugin->interfaceType == PLUGIN_TYPE_VST_2X) {
+      setVst2xPluginChunk(plugin, chunk);
+      free(chunk);
+      return true;
+    }
+    else {
+      logInternalError("Load FXP preset to wrong plugin type");
+      return false;
+    }
   }
   else {
     logInternalError("Invalid FXP program type");
