ebd30d7ff2e2dbdad54a56ce6fd8f7005a35e7d6
Nik Reiman <nik.reiman@gmail.com>
2012-09-04 17:33:50 +0200

    Add new test for processing with again plugin

--- test/MrsWatsonTest.c
+++ test/MrsWatsonTest.c
@@ -1,13 +1,36 @@
 #include "ApplicationRunner.h"
 
+static char* _getTestInputFilename(const char* fileExtension) {
+  CharString filename = newCharString();
+  snprintf(filename->data, filename->capacity, "resources%caudio%ca440-stereo.%s",
+    PATH_DELIMITER, PATH_DELIMITER, fileExtension);
+  return filename->data;
+}
+
 static LinkedList _argsForScanPlugins(void) {
   LinkedList args = newLinkedList();
   appendItemToList(args, "--list-plugins");
   return args;
 }
 
+static LinkedList _argsForProcessWithAgainPlugin(void) {
+  LinkedList args = newLinkedList();
+  appendItemToList(args, "--plugin");
+  appendItemToList(args, "again");
+  appendItemToList(args, "--input");
+  appendItemToList(args, _getTestInputFilename("pcm"));
+  return args;
+}
+
 void runMrsWatsonTests(char *applicationPath);
 void runMrsWatsonTests(char *applicationPath) {
-  runApplicationTest(applicationPath, "Run with no plugins", newLinkedList(), RETURN_CODE_MISSING_REQUIRED_OPTION, NULL);
-  runApplicationTest(applicationPath, "List plugins", _argsForScanPlugins(), RETURN_CODE_SUCCESS, NULL);
+  runApplicationTest(applicationPath,
+    "Run with no plugins", newLinkedList(),
+    RETURN_CODE_MISSING_REQUIRED_OPTION, false);
+  runApplicationTest(applicationPath,
+    "List plugins", _argsForScanPlugins(),
+    RETURN_CODE_NOT_RUN, false);
+  runApplicationTest(applicationPath,
+    "Process with again plugin", _argsForProcessWithAgainPlugin(),
+    RETURN_CODE_SUCCESS, true);
 }
