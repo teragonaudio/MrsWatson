760452345a9b1cecd28ee0ebb3f96b5a6d5b982b
Nik Reiman <nik.reiman@gmail.com>
2012-09-09 11:28:48 +0200

    Uncompress vendor tarballs

--- setup-build.sh
+++ setup-build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+for x in vendor/*.gz ; do
+  libFile=$(basename $x)
+  libName=$(printf "%s" "$libFile" | cut -d '-' -f 1)
+  outDir=$(printf "%s" "$libFile" | cut -d '.' -f -3)
+  (cd vendor ; tar xfz "$libFile" ; ln -s "$outDir" "$libName")
+done
+
 aclocal
 autoheader
 automake --add-missing
