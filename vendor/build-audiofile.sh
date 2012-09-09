c81946d9ab325d970aa2f4db253fd8d5f46a3180
Nik Reiman <nik.reiman@gmail.com>
2012-09-09 12:13:51 +0200

    Add script to build audiofile

---
+++ vendor/build-audiofile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+(cd audiofile
+./configure CFLAGS="-arch i386 -m32" CPPFLAGS="-arch i386 -m32" \
+  CXXFLAGS="-arch i386 -m32" LDFLAGS="-arch i386 -m32" \
+  --enable-static
+make clean
+make
+)
