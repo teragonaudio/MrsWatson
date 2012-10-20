14d2ae7010f4659c9499109bddbbabee380e4695
Nik Reiman <nik.reiman@gmail.com>
2012-10-20 23:30:29 +0200

    Implement some tests in StringUtilities

--- test/StringUtilitiesTest.c
+++ test/StringUtilitiesTest.c
@@ -2,18 +2,23 @@
 #include "StringUtilities.h"
 
 static int _testIsLetter(void) {
+  assert(isLetter('a'));
+  assert(isLetter('A'));
   return 0;
 }
 
 static int _testIsNotLetter(void) {
+  assertFalse(isLetter('0'));
   return 0;
 }
 
 static int _testIsNumber(void) {
+  assert(isNumber('0'));
   return 0;
 }
 
 static int _testIsNotNumber(void) {
+  assertFalse(isNumber('a'));
   return 0;
 }
 
@@ -40,10 +45,10 @@ static int _testWrapStringLongerThanLine(void) {
 TestSuite addStringUtilitiesTests(void);
 TestSuite addStringUtilitiesTests(void) {
   TestSuite testSuite = newTestSuite("StringUtilities", NULL, NULL);
-  addTest(testSuite, "IsLetter", NULL); // _testIsLetter);
-  addTest(testSuite, "IsNotLetter", NULL); // _testIsNotLetter);
-  addTest(testSuite, "IsNumber", NULL); // _testIsNumber);
-  addTest(testSuite, "IsNotNumber", NULL); // _testIsNotNumber);
+  addTest(testSuite, "IsLetter", _testIsLetter);
+  addTest(testSuite, "IsNotLetter", _testIsNotLetter);
+  addTest(testSuite, "IsNumber", _testIsNumber);
+  addTest(testSuite, "IsNotNumber", _testIsNotNumber);
   addTest(testSuite, "WrapNullSourceString", NULL); // _testWrapNullSourceString);
   addTest(testSuite, "WrapNullDestString", NULL); // _testWrapNullDestString);
   addTest(testSuite, "WrapString", NULL); // _testWrapString);
