e901903445d067d67d9940d40690ae891a3196b6
Nik Reiman <nik.reiman@gmail.com>
2012-10-21 00:31:21 +0200

    Add some tests in PluginPresetTest

--- test/PluginPresetTest.c
+++ test/PluginPresetTest.c
@@ -1,15 +1,22 @@
 #include "TestRunner.h"
 #include "PluginPreset.h"
 
+const char* TEST_PRESET_FILENAME = "test.fxp";
+
 static int _testGuessPluginPresetType(void) {
+  assertIntEquals(guessPluginPresetType(newCharStringWithCString(TEST_PRESET_FILENAME)), PRESET_TYPE_FXP);
   return 0;
 }
 
 static int _testGuessPluginPresetTypeInvalid(void) {
+  assertIntEquals(guessPluginPresetType(newCharStringWithCString("invalid")), PRESET_TYPE_INVALID);
   return 0;
 }
 
 static int _testNewObject(void) {
+  PluginPreset p = newPluginPreset(PRESET_TYPE_FXP, newCharStringWithCString(TEST_PRESET_FILENAME));
+  assertIntEquals(p->presetType, PRESET_TYPE_FXP);
+  assertCharStringEquals(p->presetName, TEST_PRESET_FILENAME);
   return 0;
 }
 
@@ -20,9 +27,9 @@ static int _testIsPresetCompatibleWithPlugin(void) {
 TestSuite addPluginPresetTests(void);
 TestSuite addPluginPresetTests(void) {
   TestSuite testSuite = newTestSuite("PluginPreset", NULL, NULL);
-  addTest(testSuite, "GuessPluginPresetType", NULL); // _testGuessPluginPresetType);
-  addTest(testSuite, "GuessPluginPresetTypeInvalid", NULL); // _testGuessPluginPresetTypeInvalid);
-  addTest(testSuite, "NewObject", NULL); // _testNewObject);
+  addTest(testSuite, "GuessPluginPresetType", _testGuessPluginPresetType);
+  addTest(testSuite, "GuessPluginPresetTypeInvalid", _testGuessPluginPresetTypeInvalid);
+  addTest(testSuite, "NewObject", _testNewObject);
   addTest(testSuite, "IsPresetCompatibleWithPlugin", NULL); // _testIsPresetCompatibleWithPlugin);
   return testSuite;
 }
