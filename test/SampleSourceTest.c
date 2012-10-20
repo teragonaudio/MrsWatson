2c46ac0cfe40bf67a00855c8cf5760e4ebd58275
Nik Reiman <nik.reiman@gmail.com>
2012-10-21 01:09:44 +0200

    Add tests for SampleSource

--- test/SampleSourceTest.c
+++ test/SampleSourceTest.c
@@ -1,33 +1,36 @@
 #include "TestRunner.h"
 #include "SampleSource.h"
 
-static int _testGuessSampleSourceTypePcm(void) {
-  return 0;
-}
+const char* TEST_SAMPLESOURCE_FILENAME = "test.pcm";
 
-static int _testGuessSampleSourceTypeNull(void) {
+static int _testGuessSampleSourceTypePcm(void) {
+  assertIntEquals(guessSampleSourceType(newCharStringWithCString(TEST_SAMPLESOURCE_FILENAME)),
+    SAMPLE_SOURCE_TYPE_PCM);
   return 0;
 }
 
 static int _testGuessSampleSourceTypeEmpty(void) {
+  assertIntEquals(guessSampleSourceType(newCharString()), SAMPLE_SOURCE_TYPE_INVALID);
   return 0;
 }
 
 static int _testGuessSampleSourceTypeInvalid(void) {
+  assertIntEquals(guessSampleSourceType(newCharString()), SAMPLE_SOURCE_TYPE_INVALID);
   return 0;
 }
 
 static int _testGuessSampleSourceTypeWrongCase(void) {
+  assertIntEquals(guessSampleSourceType(newCharStringWithCString("TEST.PCM")),
+    SAMPLE_SOURCE_TYPE_PCM);
   return 0;
 }
 
 TestSuite addSampleSourceTests(void);
 TestSuite addSampleSourceTests(void) {
   TestSuite testSuite = newTestSuite("SampleSource", NULL, NULL);
-  addTest(testSuite, "GuessSampleSourceTypePcm", NULL); // _testGuessSampleSourceTypePcm);
-  addTest(testSuite, "GuessSampleSourceTypeNull", NULL); // _testGuessSampleSourceTypeNull);
-  addTest(testSuite, "GuessSampleSourceTypeEmpty", NULL); // _testGuessSampleSourceTypeEmpty);
-  addTest(testSuite, "GuessSampleSourceTypeInvalid", NULL); // _testGuessSampleSourceTypeInvalid);
-  addTest(testSuite, "GuessSampleSourceTypeWrongCase", NULL); // _testGuessSampleSourceTypeWrongCase);
+  addTest(testSuite, "GuessSampleSourceTypePcm", _testGuessSampleSourceTypePcm);
+  addTest(testSuite, "GuessSampleSourceTypeEmpty", _testGuessSampleSourceTypeEmpty);
+  addTest(testSuite, "GuessSampleSourceTypeInvalid", _testGuessSampleSourceTypeInvalid);
+  addTest(testSuite, "GuessSampleSourceTypeWrongCase", _testGuessSampleSourceTypeWrongCase);
   return testSuite;
 }
