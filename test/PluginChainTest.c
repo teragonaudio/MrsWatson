8271149d7b04fd1d6ea94dad6b50b402cd47e080
Nik Reiman <nik.reiman@gmail.com>
2012-10-21 15:10:48 +0200

    Add more tests for PluginChain

--- test/PluginChainTest.c
+++ test/PluginChainTest.c
@@ -45,24 +45,55 @@ static int _testAddPluginFromArgumentString(void) {
   CharString testArgs = newCharStringWithCString(TEST_PLUGIN_NAME);
   assert(addPluginsFromArgumentString(p, testArgs, NULL));
   assertIntEquals(p->numPlugins, 1);
+  assertNotNull(p->plugins[0]);
   assertIntEquals(p->plugins[0]->pluginType, PLUGIN_TYPE_INTERNAL);
   assertCharStringEquals(p->plugins[0]->pluginName, "passthru");
   return 0;
 }
 
 static int _testAddPluginsFromArgumentString(void) {
+  PluginChain p = newPluginChain();
+  CharString testArgs = newCharStringWithCString(TEST_PLUGIN_NAME);
+  int i;
+
+  assert(addPluginsFromArgumentString(p, testArgs, NULL));
+  assert(addPluginsFromArgumentString(p, testArgs, NULL));
+  assertIntEquals(p->numPlugins, 2);
+  for(i = 0; i < p->numPlugins; i++) {
+    assertNotNull(p->plugins[i]);
+    assertIntEquals(p->plugins[i]->pluginType, PLUGIN_TYPE_INTERNAL);
+    assertCharStringEquals(p->plugins[i]->pluginName, "passthru");
+  }
+
   return 0;
 }
 
 static int _testAddPluginWithPresetFromArgumentString(void) {
+  PluginChain p = newPluginChain();
+  CharString testArgs = newCharStringWithCString("mrswatson:passthru,testPreset.fxp");
+  assert(addPluginsFromArgumentString(p, testArgs, NULL));
+  assertIntEquals(p->numPlugins, 1);
+  assertIntEquals(p->plugins[0]->pluginType, PLUGIN_TYPE_INTERNAL);
+  assertCharStringEquals(p->plugins[0]->pluginName, "passthru");
+  assertNotNull(p->presets[0]);
+  assertCharStringEquals(p->presets[0]->presetName, "testPreset.fxp");
   return 0;
 }
 
 static int _testAddPluginFromArgumentStringWithSpaces(void) {
+  // Hmm, I'm not sure how to test this without some really ugly hacks to Plugin.c
   return 0;
 }
 
 static int _testAddPluginFromArgumentStringWithPresetSpaces(void) {
+  PluginChain p = newPluginChain();
+  CharString testArgs = newCharStringWithCString("mrswatson:passthru,test preset.fxp");
+  assert(addPluginsFromArgumentString(p, testArgs, NULL));
+  assertIntEquals(p->numPlugins, 1);
+  assertIntEquals(p->plugins[0]->pluginType, PLUGIN_TYPE_INTERNAL);
+  assertCharStringEquals(p->plugins[0]->pluginName, "passthru");
+  assertNotNull(p->presets[0]);
+  assertCharStringEquals(p->presets[0]->presetName, "test preset.fxp");
   return 0;
 }
 
@@ -93,7 +124,7 @@ TestSuite addPluginChainTests(void) {
   addTest(testSuite, "AddPluginFromArgumentString", _testAddPluginFromArgumentString);
   addTest(testSuite, "AddPluginsFromArgumentString", _testAddPluginsFromArgumentString);
   addTest(testSuite, "AddPluginWithPresetFromArgumentString", _testAddPluginWithPresetFromArgumentString);
-  addTest(testSuite, "AddPluginFromArgumentStringWithSpaces", _testAddPluginFromArgumentStringWithSpaces);
+  addTest(testSuite, "AddPluginFromArgumentStringWithSpaces", NULL); //_testAddPluginFromArgumentStringWithSpaces);
   addTest(testSuite, "AddPluginFromArgumentStringWithPresetSpaces", _testAddPluginFromArgumentStringWithPresetSpaces);
   addTest(testSuite, "GetMaximumTailTime", NULL); // _testGetMaximumTailTime);
   addTest(testSuite, "ProcessPluginChainAudio", NULL); // _testProcessPluginChainAudio);
