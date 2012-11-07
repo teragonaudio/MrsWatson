9df37f21f8034e5c0dde01d9813cd7774725c48c
Nik Reiman <nik.reiman@gmail.com>
2012-11-07 17:06:08 +0100

    Separate tests for silence in one channel
    This is actually a rather common plugin error to make, and one that is
    worth testing for. Also fixes some test case logic which was previously
    failing.

--- test/AnalysisSilenceTest.c
+++ test/AnalysisSilenceTest.c
@@ -9,11 +9,24 @@ static int _testAnalysisSilence(void) {
 }
 
 static int _testAnalysisNotSilence(void) {
+  SampleBuffer s = newSampleBuffer(2, 8);
+  AnalysisFunctionData d = newAnalysisFunctionData();
+  int i, j;
+  for(i = 0; i < s->blocksize; i++) {
+    for(j = 0; j < s->numChannels; j++) {
+      s->samples[j][i] = 32767.0;
+    }
+  }
+  assert(analysisSilence(s, d));
+  return 0;
+}
+
+static int _testAnalysisNotSilenceInOneChannel(void) {
   SampleBuffer s = newSampleBuffer(1, 8);
   AnalysisFunctionData d = newAnalysisFunctionData();
   int i;
   for(i = 0; i < s->blocksize; i++) {
-    s->samples[0][i] = 32767;
+    s->samples[0][i] = 32767.0;
   }
   assert(analysisSilence(s, d));
   return 0;
@@ -24,5 +37,6 @@ TestSuite addAnalysisSilenceTests(void) {
   TestSuite testSuite = newTestSuite("AnalysisSilence", NULL, NULL);
   addTest(testSuite, "AnalysisSilence", _testAnalysisSilence);
   addTest(testSuite, "AnalysisNotSilence", _testAnalysisNotSilence);
+  addTest(testSuite, "AnalysisNotSilenceInOneChannel", _testAnalysisNotSilenceInOneChannel);
   return testSuite;
 }
