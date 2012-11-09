2a932c317c2ed0116a47e639745796ac10b66ab6
Nik Reiman <nik.reiman@gmail.com>
2012-11-09 20:02:53 +0100

    Version 0.9.3

--- source/MrsWatson.h
+++ source/MrsWatson.h
@@ -37,7 +37,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 9
-#define VERSION_PATCH 2
+#define VERSION_PATCH 3
 
 #define OFFICIAL_WEBSITE "http://www.teragonaudio.com"
 #define PROJECT_WEBSITE "https://github.com/teragonaudio/mrswatson"
