4996de002ae061b39755b544137fcceed4f57cc0
Nik Reiman <nik.reiman@gmail.com>
2013-05-15 17:00:23 +0200

    Clean up MidiSequence tests

--- test/sequencer/MidiSequenceTest.c
+++ test/sequencer/MidiSequenceTest.c
@@ -75,40 +75,6 @@ static int _testFillEventsSequentially(void) {
   return 0;
 }
 
-static int _testFillEventsOutOfOrder(void) {
-  MidiSequence m = newMidiSequence();
-  MidiEvent e = newMidiEvent();
-  MidiEvent e2 = newMidiEvent();
-  LinkedList l = newLinkedList();
-  e->status = 0xf7;
-  e->timestamp = 100;
-  e2->status = 0xf7;
-  e2->timestamp = 300;
-  appendMidiEventToSequence(m, e);
-  appendMidiEventToSequence(m, e2);
-  assertFalse(fillMidiEventsFromRange(m, 256, 256, l));
-  assertIntEquals(numItemsInList(l), 1);
-  assert(fillMidiEventsFromRange(m, 0, 256, l));
-  assertIntEquals(numItemsInList(l), 1);
-  return 0;
-}
-
-static int _testFillEventsFromMiddleOfRange(void) {
-  MidiSequence m = newMidiSequence();
-  MidiEvent e = newMidiEvent();
-  MidiEvent e2 = newMidiEvent();
-  LinkedList l = newLinkedList();
-  e->status = 0xf7;
-  e->timestamp = 100;
-  e2->status = 0xf7;
-  e2->timestamp = 300;
-  appendMidiEventToSequence(m, e);
-  appendMidiEventToSequence(m, e2);
-  assert(fillMidiEventsFromRange(m, 100, 256, l));
-  assertIntEquals(numItemsInList(l), 2);
-  return 0;
-}
-
 static int _testFillEventsFromRangePastSequence(void) {
   MidiSequence m = newMidiSequence();
   MidiEvent e = newMidiEvent();
@@ -116,6 +82,10 @@ static int _testFillEventsFromRangePastSequence(void) {
   e->status = 0xf7;
   e->timestamp = 100;
   appendMidiEventToSequence(m, e);
+  // Should return false since this is the last event in the sequence
+  assertFalse(fillMidiEventsFromRange(m, 0, 200, l));
+  assertIntEquals(numItemsInList(l), 1);
+  l = newLinkedList();
   assertFalse(fillMidiEventsFromRange(m, 200, 256, l));
   assertIntEquals(numItemsInList(l), 0);
   return 0;
@@ -132,11 +102,7 @@ TestSuite addMidiSequenceTests(void) {
   addTest(testSuite, "FillEventsFromRangeStart", _testFillMidiEventsFromRangeStart);
   addTest(testSuite, "FillEventsFromEmptyRange", _testFillEventsFromEmptyRange);
   addTest(testSuite, "FillEventsSequentially", _testFillEventsSequentially);
-
-  // TODO: These tests fail because MidiSequence is meant to be read sequentially from start to finish
-  addTest(testSuite, "FillEventsOutOfOrder", NULL); // _testFillEventsOutOfOrder);
-  addTest(testSuite, "FillEventsFromMiddleOfRange", NULL); // _testFillEventsFromMiddleOfRange);
-  addTest(testSuite, "FillEventsFromRangePastSequenceEnd", NULL); // _testFillEventsFromRangePastSequence);
+  addTest(testSuite, "FillEventsFromRangePastSequenceEnd", _testFillEventsFromRangePastSequence);
 
   return testSuite;
 }
