43de770697509686cf6e6323c1e537910dc4109d
Nik Reiman <nik.reiman@gmail.com>
2013-05-19 21:49:15 +0200

    Add function to flatten LinkedList to array
    Man, I've been needing this for a looong time, not sure what took me so long
    to write this fairly trivial code.

--- test/base/LinkedListTest.c
+++ test/base/LinkedListTest.c
@@ -162,6 +162,36 @@ static int _testLinkedListWithEmptyList(void) {
   return 0;
 }
 
+static int _testLinkedListToArray(void) {
+  LinkedList l = newLinkedList();
+  CharString* arr;
+
+  appendItemToList(l, newCharStringWithCString("one"));
+  appendItemToList(l, newCharStringWithCString("two"));
+  arr = (CharString*)linkedListToArray(l);
+  assertNotNull(arr);
+  assertCharStringEquals((CharString)arr[0], "one");
+  assertCharStringEquals((CharString)arr[1], "two");
+  assertIsNull(arr[2]);
+
+  return 0;
+}
+
+static int _testLinkedListToArrayWithNull(void) {
+  CharString** arr;
+  arr = (CharString**)linkedListToArray(NULL);
+  assertIsNull(arr);
+  return 0;
+}
+
+static int _testLinkedListWithEmptyList(void) {
+  CharString** arr;
+  LinkedList l = newLinkedList();
+  arr = (CharString**)linkedListToArray(l);
+  assertIsNull(arr);
+  return 0;
+}
+
 static void _linkedListEmptyCallback(void* item, void* userData) {
   _gNumForeachCallbacksMade++;
 }
