656b3b6c4ad82e2b70997b9f59ea3e0d7281ed97
Nik Reiman <nik.reiman@gmail.com>
2013-05-26 12:31:14 +0200

    Revert "Remove unused variables"
    This reverts commit cf5f7f8dbbc707eac8c3e6ef47b6046a7bb35b74.

--- source/plugin/PluginChain.c
+++ source/plugin/PluginChain.c
@@ -283,6 +283,8 @@ void pluginChainShutdown(PluginChain pluginChain) {
 }
 
 void freePluginChain(PluginChain pluginChain) {
+  Plugin plugin;
+  PluginPreset preset;
   int i;
 
   for(i = 0; i < pluginChain->numPlugins; i++) {
