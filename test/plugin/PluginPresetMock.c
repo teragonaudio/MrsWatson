54604847ad2cc17f694e62f23630ebbbc29143b9
Nik Reiman <nik.reiman@gmail.com>
2013-06-07 17:14:52 +0200

    Improve plugin chain tests, implement all stubs

---
+++ test/plugin/PluginPresetMock.c
@@ -0,0 +1,38 @@
+#include "PluginPresetMock.h"
+
+static boolByte _openPluginPresetMock(void* pluginPresetPtr) {
+  PluginPreset pluginPreset = (PluginPreset)pluginPresetPtr;
+  PluginPresetMockData extraData = (PluginPresetMockData)pluginPreset->extraData;
+  extraData->isOpen = true;
+  return true;
+}
+
+static boolByte _loadPluginPresetMock(void* pluginPresetPtr, Plugin plugin) {
+  PluginPreset pluginPreset = (PluginPreset)pluginPresetPtr;
+  PluginPresetMockData extraData = (PluginPresetMockData)pluginPreset->extraData;
+  extraData->isLoaded = true;
+  return true;
+}
+
+static void _freePluginPresetMock(void* extraDataPtr) {
+}
+
+PluginPreset newPluginPresetMock(void) {
+  PluginPreset pluginPreset = (PluginPreset)malloc(sizeof(PluginPresetMembers));
+  PluginPresetMockData extraData = (PluginPresetMockData)malloc(sizeof(PluginPresetMockDataMembers));
+
+  pluginPreset->presetType = PRESET_TYPE_INTERNAL_PROGRAM;
+  pluginPreset->presetName = newCharString();
+  pluginPreset->compatiblePluginTypes = 0;
+  _pluginPresetSetCompatibleWith(pluginPreset, PLUGIN_TYPE_INTERNAL);
+
+  pluginPreset->openPreset = _openPluginPresetMock;
+  pluginPreset->loadPreset = _loadPluginPresetMock;
+  pluginPreset->freePresetData = _freePluginPresetMock;
+
+  extraData->isOpen = false;
+  extraData->isLoaded = false;
+  pluginPreset->extraData = extraData;
+
+  return pluginPreset;  
+}
---
+++ test/plugin/PluginPresetMock.h
@@ -0,0 +1,15 @@
+#ifndef MrsWaston_PluginPresetMock_h
+#define MrsWaston_PluginPresetMock_h
+
+#include "base/Types.h"
+#include "plugin/PluginPreset.h"
+
+typedef struct {
+  boolByte isOpen;
+  boolByte isLoaded;
+} PluginPresetMockDataMembers;
+typedef PluginPresetMockDataMembers* PluginPresetMockData;
+
+PluginPreset newPluginPresetMock(void);
+
+#endif
