b89c18fe5d777d0b024ad00d9aa6aac44e0b3458
Nik Reiman <nik.reiman@gmail.com>
2016-07-01 15:04:38 +0200

    Cleanups to CMake build files
    - Fix variable naming
    - Move copy/pasted code to functions
    - Clean up indenting, other formatting stuff
    - Intermediate libraries have wordsize in name
    - Fix shared build flags inclusion on unix (untested!)

--- cmake/ConfigureTarget.cmake
+++ cmake/ConfigureTarget.cmake
@@ -1,26 +1,45 @@
 cmake_minimum_required(VERSION 3.0)
 
-function(configure_target target_NAME target_BITNESS)
+# Configure a target to set the correct properties for 32/64-bit builds
+function(configure_target target wordsize)
   if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-    if(${target_BITNESS} EQUAL 32)
-      set_target_properties(${target_NAME} PROPERTIES COMPILE_FLAGS "-m32 -std=c99")
-      set_target_properties(${target_NAME} PROPERTIES LINK_FLAGS "-m32")
+    if(${wordsize} EQUAL 32)
+      set_target_properties(${target} PROPERTIES
+        COMPILE_FLAGS "-m32 -std=c99"
+      )
+      set_target_properties(${target} PROPERTIES
+        LINK_FLAGS "-m32"
+      )
     else()
-      set_target_properties(${target_NAME} PROPERTIES COMPILE_FLAGS "-m64 -std=c99")
-      set_target_properties(${target_NAME} PROPERTIES LINK_FLAGS "-m64")
+      set_target_properties(${target} PROPERTIES
+        COMPILE_FLAGS "-m64 -std=c99"
+      )
+      set_target_properties(${target} PROPERTIES
+        LINK_FLAGS "-m64"
+      )
     endif()
-    target_link_libraries(${target_NAME} dl)
+    target_link_libraries(${target} dl)
+
   elseif(APPLE)
-    if(${target_BITNESS} EQUAL 32)
-      set_target_properties(${target_NAME} PROPERTIES OSX_ARCHITECTURES "i386")
+    if(${wordsize} EQUAL 32)
+      set_target_properties(${target} PROPERTIES
+        OSX_ARCHITECTURES "i386"
+      )
     else()
-      set_target_properties(${target_NAME} PROPERTIES OSX_ARCHITECTURES "x86_64")
+      set_target_properties(${target} PROPERTIES
+        OSX_ARCHITECTURES "x86_64"
+      )
     endif()
+
   elseif(MSVC)
-    if(${target_BITNESS} EQUAL 32)
-      set_target_properties(${target_NAME} PROPERTIES COMPILE_FLAGS "/DWIN32=1")
+    if(${wordsize} EQUAL 32)
+      set_target_properties(${target} PROPERTIES
+        COMPILE_FLAGS "/DWIN32=1"
+      )
     else()
-      set_target_properties(${target_NAME} PROPERTIES COMPILE_FLAGS "/DWIN64=1")
+      set_target_properties(${target} PROPERTIES
+        COMPILE_FLAGS "/DWIN64=1"
+      )
     endif()
   endif()
 endfunction()
